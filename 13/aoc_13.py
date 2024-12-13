ecf701b78dbd5693a13d722d4496aec41c4d7204
Hannes <hannes-bachmann@t-online.de>
2024-12-13 23:29:39 +0100

    result for day 13 second task

--- 13/aoc_13.py
+++ 13/aoc_13.py
@@ -35,7 +35,28 @@ def task_01(inputs):
 
 
 def task_02(inputs):
-    total = 0
+    games = [inputs[i:i+3] for i in range(0, len(inputs), 3)]
+    games_nums = [[[int(d) for d in re.findall(r'[0-9]+', g)] for g in game] for game in games]
+
+    # calculate number of button A or B presses
+    get_a = lambda Ax, Bx, Ay, By, X, Y: (-1*Bx*Y + By*X) / (Ax*By - Bx*Ay)
+    get_b = lambda Ax, Bx, Ay, By, X, Y: (Ax*Y - Ay*X) / (Ax*By - Bx*Ay)
+
+    solvable = []
+    costs = []
+    for (Ax, Ay), (Bx, By), (X, Y) in games_nums:
+        X_new = X + 10000000000000
+        Y_new = Y + 10000000000000
+        a = get_a(Ax, Bx, Ay, By, X_new, Y_new)
+        b = get_b(Ax, Bx, Ay, By, X_new, Y_new)
+        if a.is_integer() and b.is_integer():
+            solvable.append(True)
+            costs.append(a*3 + b*1)
+        else:
+            solvable.append(False)
+        pass
+    total = int(sum(costs))
+
     return total
 
 
