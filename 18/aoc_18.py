2326aa173706a52c8595ba158e791f917087a740
Hannes <hannes-bachmann@t-online.de>
2024-12-18 16:10:57 +0100

    result for day 18 second task, speed up version

--- 18/aoc_18.py
+++ 18/aoc_18.py
@@ -1,6 +1,7 @@
 import numpy as np
 import re
 from a_star import a_star_search
+import time
 
 
 def read_input():
@@ -48,24 +49,32 @@ def task_02(inputs):
     dest = [70, 70]  # personal input destination
     # dest = [6, 6]  # example destination
     last_pos = None
+    time_st = time.time()
+    path = []
     for r, c in mem_locations:
         mem_space[r][c] = 0  # blocked cell (corrupted bit)
-        if i >= 1024:       # as known from task 1
+        if i == 0 or str((c, r)) in path:
+            # consider only corruptions which intersect with the last found path -> calculate new path
             path = a_star_search(mem_space, src, dest)
             if path is None:
                 print(f'bit {i} at position ({c}, {r})')
                 last_pos = [c, r]
                 break
             else:
+                path = [str(pos) for pos in path]
                 print(f'path until corruption {i} found')
         i += 1
         pass
+    time_passed = time.time() - time_st
 
     return last_pos
+# (22, 20)
+# version 1: 235.8 sec
+# version 2: 8.9
 
 
 if __name__ == '__main__':
     inputs = read_input()
-    t1_solution = task_01(inputs)
+    # t1_solution = task_01(inputs)
     t2_solution = task_02(inputs)
     pass
